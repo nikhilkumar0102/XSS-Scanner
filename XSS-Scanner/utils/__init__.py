fc87a00d6d012ed404bd79489218676f9a9b89a8
Nikhil Kumar <nikhilkumar01.name@gmail.com>
2025-11-24 02:00:15 +0530

    Delete XSS-Scanner directory

--- XSS-Scanner/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Utility functions and helpers for XSS Scanner.
-"""
-
-from .colors import Colors, Banner, ProgressBar
-
-__all__ = ['Colors', 'Banner', 'ProgressBar']
-                                                
